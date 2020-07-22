35078d1a4208b0bfd1be9f028e933e1e406941c5
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-22 02:26:15 -0700

    Added user questions with function to be declared.

--- employee-tracker.js
+++ employee-tracker.js
@@ -14,5 +14,54 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
   
-    firstPrompt();
+    start();
   });
+
+  function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View All Employees":
+          employeeSearch();
+          break;
+  
+        case "View All Employees By Department":
+          employeeByDeptSearch();
+          break;
+  
+        case "View All Employees By Manager":
+          employeeByManager();
+          break;
+  
+        case "Add Employee":
+          addEmployee();
+          break;
+  
+        case "Remove Employee":
+          removeEmployee();
+          break;
+
+        case "Add Employee Role":
+          employeeRole();
+          break;
+
+        case "Add Employee Manager":
+          updateEmployeeManager();
+          break;
+        }
+      });
+  }
