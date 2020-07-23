8f517c198e05246398c3a485150d9ee355fcbe64
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-22 21:33:03 -0700

    Input asyn function to view dept/roles/employees,app functioning

--- employee-tracker.js
+++ employee-tracker.js
@@ -8,60 +8,110 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "password",
-  database: "employeesDB"
+  database: "employees_DB"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
   
-    start();
+    promptStart();
   });
 
-  function start() {
+  function promptStart() {
     inquirer
       .prompt({
         name: "action",
         type: "rawlist",
         message: "What would you like to do?",
         choices: [
+          "View All Departments",
+          "View All Roles",
           "View All Employees",
-          "View All Employees By Department",
-          "View All Employees By Manager",
+          "Add Department",
+          "Add Role",
           "Add Employee",
-          "Remove Employee",
           "Update Employee Role",
-          "Update Employee Manager",
         ]
       })
       .then(function(answer) {
         switch (answer.action) {
+        case "View All Departments":
+          viewAllDepts();
+          break;
+  
+        case "View All Roles":
+          viewAllRoles();
+          break;
+  
         case "View All Employees":
-          employeeSearch();
+          viewAllEmployees();
           break;
-  
-        case "View All Employees By Department":
-          employeeByDeptSearch();
+
+        case "Add Department":
+          addDept();
           break;
-  
-        case "View All Employees By Manager":
-          employeeByManager();
+
+        case "Add Role":
+          addRole();
           break;
-  
+
         case "Add Employee":
           addEmployee();
           break;
-  
-        case "Remove Employee":
-          removeEmployee();
-          break;
 
-        case "Add Employee Role":
-          employeeRole();
-          break;
-
-        case "Add Employee Manager":
-          updateEmployeeManager();
-          break;
+        case "Update Employee Role":
+            updateEmployeeRole();
+                  break;
         }
       });
   }
+
+   async function viewAllDepts(){
+    connection.query ("SELECT * FROM department", function(err, res){
+      console.table(res);
+      promptStart();
+    })
+    }
+  // async function viewAllDepts() {
+  //   var query = "SELECT * FROM department";
+  //     connection.query(query, function(err, res) {
+  //         console.log(`DEPARTMENTS:`)
+  //       res.forEach(department => {
+  //           console.log(`ID: ${department.id} | Name: ${department.name}`)
+  //       })
+  //       start();
+  //       });
+  //   };
+   async function viewAllRoles(){
+     connection.query("SELECT role.*, department.name FROM role LEFT JOIN department ON department.id = role.department_id", async function (err,res){
+      if (err) throw err;
+      console.table(res);
+      promptStart();
+    })}
+    // async function viewAllRoles() {
+    // var query = "SELECT * FROM role";
+    //     connection.query(query, function(err, res) {
+    //         console.log(`ROLES:`)
+    //     res.forEach(role => {
+    //         console.log(`ID: ${role.id} | Title: ${role.title} | Salary: ${role.salary} | Department ID: ${role.department_id}`);
+    //     })
+    //     start();
+    //     });
+    async function viewAllEmployees() {
+      connection.query("SELECT employee.first_name, employee.last_name, role.title AS \"role\", manager.first_name AS \"manager\" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON employee.manager_id = manager.id GROUP BY employee.id",  
+      async function(err, res) {
+          if (err) throw err;
+        
+          console.table(res);
+          promptStart();
+        });
+      }
+      // async function viewAllEmployeesByManager(managerId) {
+      //   return this.connection.query(
+      //     "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      //     managerId
+      //   );
+      // }
+  
+
+    
--- employee-tracker.sql
+++ employee-tracker.sql
@@ -26,3 +26,7 @@ CREATE TABLE employee (
   manager_id INT NULL,
   PRIMARY KEY (id)
 );
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM department;
